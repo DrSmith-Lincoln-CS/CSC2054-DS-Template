6b0908056428264ddbf0267350eecbd634fe817a
DrTiff-Lincoln <tsmith1.lincoln@gmail.com>
2026-01-12 16:02:09 -0500

    Update lab1_part1.cpp
    Added the assignment part

--- week2/lab1_part1.cpp
+++ week2/lab1_part1.cpp
@@ -4,7 +4,9 @@ using namespace std;
 
 int main()
 {
+    
     int n;
+    //Add a display statement to ask the user for the number
     cin >> n;
     cout<<"The number is: "<<n<<endl;
     return 0;
