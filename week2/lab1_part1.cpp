c87120e66826dfb1db3bd5802c5d3048dafac9f5
DrTiff-Lincoln <tsmith1.lincoln@gmail.com>
2026-01-12 16:10:52 -0500

    Added additional requirements

--- week2/lab1_part1.cpp
+++ week2/lab1_part1.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-    
+    //Add your name and favorite food as two variables
     int n;
     //Add a display statement to ask the user for the number
     cin >> n;
